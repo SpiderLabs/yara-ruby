3bec91bbdf1b7a1fcba9a9d627d07b4b7e82a1b8
Eric Monti <esmonti@gmail.com>
2011-01-11 15:56:27 -0600

    helpers and sugar in ruby

--- lib/yara.rb
+++ lib/yara.rb
@@ -1,2 +1,17 @@
 
 require 'yara_native'
+
+module Yara
+  class Rules
+    class Match
+    end
+
+    class MatchString
+
+      def <=>(other)
+        self.offset <=> other.offset
+      end
+    end
+  end
+
+end
