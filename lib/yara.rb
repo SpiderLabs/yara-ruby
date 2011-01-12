5df409749d72d9f75aae68dd13a6695f1edd9620
Eric Monti <esmonti@gmail.com>
2011-01-11 22:10:32 -0600

    inspect and array/hash helpers

--- lib/yara.rb
+++ lib/yara.rb
@@ -6,12 +6,39 @@ module Yara
   end
 
   class Match
+    def to_hash
+      { :rule => self.rule, 
+        :namespace => self.namespace, 
+        :tags => self.tags,
+        :meta => self.meta,
+        :strings => self.strings }
+    end
+
+    def inspect
+      h=to_hash
+      h.inspect
+    end
   end
 
   class MatchString
 
+    alias ident identifier
+
     def <=>(other)
       self.offset <=> other.offset
+    end
+
+    def to_a
+      [self.offset, self.ident, self.buffer]
+    end
+
+    def to_hash
+      { :offset => self.offset, :identifier => self.ident, :buffer => self.buffer}
+    end
+
+    def inspect
+      h=to_a
+      h.inspect
     end
   end
 
